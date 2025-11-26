e0733e272a7ea85029e5093644baf3e6c7b97d8b
Benjamin Layet <layetbenjamin@gmail.com>
2025-11-26 19:33:35 +0100

    fixes counter example UI

--- packages/examples/basic-example-counter/src/components/counter/Counter.tsx
+++ packages/examples/basic-example-counter/src/components/counter/Counter.tsx
@@ -6,18 +6,23 @@ export const Counter = ({ path = "/" }) => {
   const [{ count }, { incrementRequested, decrementRequested }] =
     useSofter<CounterContract>(path);
 
-  console.log("Counter render", { path, count });
-  const [test, setTest] = useState(0);
   return (
-    <div className="horizontal">
-      <button aria-label="Decrement value" onClick={() => decrementRequested()}>
-        -
-      </button>
-      <label>{count}</label>
-      <button aria-label="Increment value" onClick={() => incrementRequested()}>
-        +
-      </button>
-      <button onClick={() => setTest(test + 1)}>Test: {test}</button>
+    <div>
+      <div style={{ fontSize: "4em" }}>{count}</div>
+      <div className="horizontal">
+        <button
+          aria-label="Decrement value"
+          onClick={() => decrementRequested()}
+        >
+          -
+        </button>
+        <button
+          aria-label="Increment value"
+          onClick={() => incrementRequested()}
+        >
+          +
+        </button>
+      </div>
     </div>
   );
 };
