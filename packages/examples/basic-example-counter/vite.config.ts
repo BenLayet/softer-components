9f4e99cc7a2e4e99b1447994ae1983f66d610587
nils <nilscox.dev@gmail.com>
2025-12-07 13:50:05 +0100

    add packages aliases in examples

--- packages/examples/basic-example-counter/vite.config.ts
+++ packages/examples/basic-example-counter/vite.config.ts
@@ -1,11 +1,12 @@
 import react from "@vitejs/plugin-react";
 import * as path from "node:path";
+import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 import packageJson from "./package.json" with { type: "json" };
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tsconfigPaths(), react()],
 
   server: {
     open: true,
