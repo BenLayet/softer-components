a3568ae627882282dfb9359e7004e9e601a50750
Benjamin Layet <layetbenjamin@gmail.com>
2026-01-16 12:10:05 +0100

    fixes selectors childrenValues type

--- packages/types/src/selectors.ts
+++ packages/types/src/selectors.ts
@@ -1,12 +1,14 @@
-import { ComponentContract } from "./component-contract";
+import { ChildInstanceContract, ComponentContract } from "./component-contract";
 import { State } from "./state";
 import { ChildrenValues } from "./values";
 
 export type Selectors<
   TState extends State,
-  TChildren extends Record<string, ComponentContract> = {},
+  TChildren extends Record<string, ComponentContract & ChildInstanceContract> =
+    {},
 > = Record<string, Selector<TState, TChildren>>;
 export type Selector<
   TState extends State,
-  TChildren extends Record<string, ComponentContract> = {},
-> = (state: TState, children: ChildrenValues<TChildren>) => any;
+  TChildren extends Record<string, ComponentContract & ChildInstanceContract> =
+    {},
+> = (state: TState, childrenValues: ChildrenValues<TChildren>) => any;
