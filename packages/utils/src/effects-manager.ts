f63226755495cdd0320d521c0835bb06dafef6d9
Benjamin Layet <layetbenjamin@gmail.com>
2026-01-28 23:40:48 +0100

    moves component path utility types to 'types' package

--- packages/utils/src/effects-manager.ts
+++ packages/utils/src/effects-manager.ts
@@ -1,8 +1,10 @@
 import {
   ComponentContract,
   ComponentDef,
+  ComponentPaths,
   Effect,
   Effects,
+  GetContractAtPath,
   Payload,
 } from "@softer-components/types";
 
@@ -14,36 +16,6 @@ import { StateReader } from "./state-manager";
 import { ComponentPath, GlobalEvent, SofterRootState } from "./utils.type";
 
 type Unregister = () => void;
-// Recursive type to get all component paths
-export type ComponentPaths<T> =
-  | "/"
-  | (T extends { children: infer C }
-      ? C extends Record<string, any>
-        ? {
-            [K in keyof C & string]:
-              | `/${K}`
-              | `/${K}${Exclude<ComponentPaths<C[K]>, "/">}`;
-          }[keyof C & string]
-        : never
-      : never);
-
-// Utility type to get ComponentContract at a specific path
-export type GetContractAtPath<T, Path extends string> = Path extends "/"
-  ? T
-  : Path extends `/${infer First}/${infer Rest}`
-    ? T extends { children: infer C }
-      ? First extends keyof C
-        ? GetContractAtPath<C[First], `/${Rest}`>
-        : never
-      : never
-    : Path extends `/${infer Only}`
-      ? T extends { children: infer C }
-        ? Only extends keyof C
-          ? C[Only]
-          : never
-        : never
-      : never;
-
 export class EffectsManager<TRootComponentContract extends ComponentContract> {
   private readonly effectsMap: {
     [componentDefPath: string]: { [eventName: string]: Effect[] };
