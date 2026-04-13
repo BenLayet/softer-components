6db172a905da23f9648ed2e94d74533bd77a0bcb
Benjamin Layet <layetbenjamin@gmail.com>
2026-04-13 12:55:47 +0200

    fixes event default payload in EventContract

--- packages/types/src/component-contract.ts
+++ packages/types/src/component-contract.ts
@@ -4,9 +4,18 @@ export type ValuesContract = { [SelectorName in string]: any };
 
 export type EventsContract<
   TAllEvents extends readonly string[] = readonly string[],
-  TPayloads extends { [T in TAllEvents[number]]?: Payload } = {
-    [T in TAllEvents[number]]?: Payload;
-  },
+  // Default type of TPayloads has 2 possible values:
+  //  - when using EventsContract without parameter: most generic (unspecified
+  //  Payload for all events)
+  //  - when usingEventsContract with just one parameter: empty object so that
+  //  ComponentContract<{events:EventContract<["clicked"]>> does extend
+  //  ComponentContract, and the payload of clicked is recognized as undefined
+  TPayloads extends { [T in TAllEvents[number]]?: Payload } =
+    string[] extends TAllEvents
+      ? {
+          [T in TAllEvents[number]]?: Payload; // necessary
+        }
+      : {},
   TUiEvents extends readonly TAllEvents[number][] = TAllEvents,
 > = { allEvents: TAllEvents; payloads: TPayloads; uiEvents: TUiEvents };
 
