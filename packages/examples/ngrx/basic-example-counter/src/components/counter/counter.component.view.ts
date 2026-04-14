73eeb1f5c88a0ef3c5fbe230a2ea26389720c2ad
Benjamin Layet <layetbenjamin@gmail.com>
2026-04-14 09:42:31 +0200

    fixes ngrx counter example (rm FormModule)

--- packages/examples/ngrx/basic-example-counter/src/components/counter/counter.component.view.ts
+++ packages/examples/ngrx/basic-example-counter/src/components/counter/counter.component.view.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
 import { CounterContract } from './counter.component';
 import { AbstractSofterComponent } from '@softer-components/ngrx-adapter';
-import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.component.html',
-  imports: [FormsModule],
 })
 export class Counter extends AbstractSofterComponent<CounterContract> {}
