42d05c9f49f4f185af7e12a071a03ae4098a9690
Benjamin Layet <layetbenjamin@gmail.com>
2026-05-03 14:09:30 +0200

    removes empty config files

--- packages/test-utilities/vite.config.ts
+++ packages/test-utilities/vite.config.ts
@@ -2,7 +2,7 @@ import dts from "vite-plugin-dts";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  plugins: [dts({ rollupTypes: true, tsconfigPath: "./tsconfig.build.json" })],
+  plugins: [dts({ rollupTypes: true })],
   build: {
     outDir: "lib",
     emptyOutDir: true,
