9b4e40948e56546c366c57a7a283f18157c25c56
Benjamin Layet <layetbenjamin@gmail.com>
2026-06-12 11:49:15 +0200

    fix(setupTests): update global variable declaration for test configuration

--- packages/examples/redux/complete-example-shopping-list/test/setupTests.ts
+++ packages/examples/redux/complete-example-shopping-list/test/setupTests.ts
@@ -1,3 +1,7 @@
 import "@testing-library/jest-dom/vitest";
 
-(globalThis as any).__SOFTER_DEBUG__ = true;
+const globalForTests = globalThis as typeof globalThis & {
+  __SOFTER_DEBUG__?: boolean;
+};
+
+globalForTests.__SOFTER_DEBUG__ = true;
